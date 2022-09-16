b9590b171dde0e1b140424ad88a9444abd5a0a08
Anam <anamsiswa01@gmail.com>
2022-09-16 10:21:01 +0700

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,121 @@
+<template>
+    <!-- Remove the container if you want to extend the Footer to full width. -->
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-white" style="background-color: #212529">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold"> Company name </h6>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem aspernatur sint ad saepe eveniet! Libero ratione distinctio numquam cumque mollitia laborum ea saepe repellendus. Vero neque natus ipsam. Dolorum, commodi. </p>
+                    </div>
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                        <p>
+                            <a class="text-white">BrandFlow</a>
+                        </p>
+                        <p>
+                            <a class="text-white">BrandFlow</a>
+                        </p>
+                        <p>
+                            <a class="text-white">BrandFlow</a>
+                        </p>
+                        <p>
+                            <a class="text-white">BrandFlow</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold"> Useful links </h6>
+                        <p>
+                            <a class="text-white">Your Account</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Become an Affiliate</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Shipping Rates</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Help</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p>
+                            <i class="fas fa-home mr-3"></i> New York, NY 10012, US
+                        </p>
+                        <p>
+                            <i class="fas fa-envelope mr-3"></i> info@gmail.com
+                        </p>
+                        <p>
+                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88
+                        </p>
+                        <p>
+                            <i class="fas fa-print mr-3"></i> + 01 234 567 89
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+            <hr class="my-3">
+            <!-- Section: Copyright -->
+            <section class="p-3 pt-0">
+                <div class="row d-flex align-items-center">
+                    <!-- Grid column -->
+                    <div class="col-md-7 col-lg-8 text-center text-md-start">
+                        <!-- Copyright -->
+                        <div class="p-3"> Copyright &copy; {{ new Date().getFullYear() }}
+                        </div>
+                        <!-- Copyright -->
+                    </div>
+                    <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                        <!-- Facebook -->
+                        <a class="btn btn-outline-light btn-floating m-1" role="button">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                        <!-- Twitter -->
+                        <a class="btn btn-outline-light btn-floating m-1" role="button">
+                            <i class="bi bi-twitter"></i>
+                        </a>
+                        <!-- Google -->
+                        <a class="btn btn-outline-light btn-floating m-1" role="button">
+                            <i class="bi bi-google"></i>
+                        </a>
+                        <!-- Instagram -->
+                        <a class="btn btn-outline-light btn-floating m-1" role="button">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+            </section>
+            <!-- Section: Copyright -->
+        </div>
+        <!-- Grid container -->
+    </footer>
+    <!-- Footer -->
+    <!-- End of .container -->
+</template>
+
+<style scoped>
+a {
+  text-decoration: none !important;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="p-5 bg-dark text-light row">
+        <div class="container-fluid py-5 col-md-6">
+            <h1 class="display-8 fw-bold">Lorem Ipsum</h1>
+            <p class="col-md-8 fs-4">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+        </div>
+        <div class="container-fluid py-5 col-md-6">
+            <img src="https://assets-global.website-files.com/5f46baf9863467a01b1aaab9/5f479acca51c91168e689008_card-portrait-tiny.png" class="img-circle" alt="Photo" style="width:90%">
+        </div>
+        <Carousel :items-to-show="3" :wrap-around="true">
+            <Slide v-for="item in this.slides" :key="`item-${item}`">
+                <!-- <div class="carousel__item">{{ slide }}</div> -->
+                <img :src="`${item}`" class="card-img-top" :alt="`cover for ${item}`">
+            </Slide>
+
+            <template #addons>
+                <Navigation />
+            </template>
+        </Carousel>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+    import { Carousel, Navigation, Slide } from 'vue3-carousel';
+    import 'vue3-carousel/dist/carousel.css';
+
+    export default defineComponent({
+        name: 'home',
+        components: {
+            Carousel,
+            Slide,
+            Navigation,
+        },
+        data: () => ({
+            slides: [
+                "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+                "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+                "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+                "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+            ]
+        })
+    });
+</script>
+
+<style scoped>  
+
+</style>
